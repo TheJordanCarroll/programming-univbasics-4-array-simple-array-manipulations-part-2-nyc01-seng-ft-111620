20846fa0f70447e22610ffd4e4b9c5217cde8e0c
Jordan Carroll <jordancarroll1994@gmail.com>
2020-11-06 01:53:03 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+end
+
+def using_insert (list_of_programming_languages, another_language)
+  another_language = "Python"
+  list_of_programming_languages.insert(4, another_language)
+end
+
+def using_uniq (haircuts)
+  haircuts.uniq
+end
+
+def using_flatten (instruments)
+  instruments.flatten 
+end
